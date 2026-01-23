ea98c4f23eee5c97212a2113eac7a2defa8bb912
ostef <oumansour.stefan@gmail.com>
2026-01-23 14:03:35 +0100

    Added coding style check, fixed coding style errors

--- ex07/hello_debugfs.c
+++ ex07/hello_debugfs.c
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
 #include <linux/module.h>
 #include <linux/printk.h>
 #include <linux/init.h>
@@ -16,53 +18,50 @@ MODULE_DESCRIPTION("Simple misc device driver");
 
 static ssize_t id_read(struct file *f, char __user *buff, size_t len, loff_t *offset)
 {
-        if (!buff) {
-                return -EINVAL;
-        }
-        if (*offset >= LOGIN_LENGTH) {
-                return 0;
-        }
+	if (!buff)
+		return -EINVAL;
+	if (*offset >= LOGIN_LENGTH)
+		return 0;
 
-        size_t copy_length = min(LOGIN_LENGTH - *offset, len);
-        size_t copied = copy_length - copy_to_user(buff, LOGIN + *offset, copy_length);
-        *offset += copied;
+	size_t copy_length = min(LOGIN_LENGTH - *offset, len);
+	size_t copied = copy_length - copy_to_user(buff, LOGIN + *offset, copy_length);
 
-        return (ssize_t)copied;
+	*offset += copied;
+
+	return (ssize_t)copied;
 }
 
 static ssize_t id_write(struct file *f, const char __user *buff, size_t len, loff_t *offset)
 {
-        char text[LOGIN_LENGTH];
+	char text[LOGIN_LENGTH];
 
-        if (!buff || len != LOGIN_LENGTH) {
-                return -EINVAL;
-        }
-        if (copy_from_user(text, buff, len) > 0) {
-                return -EIO;
-        }
-        if (memcmp(text, LOGIN, LOGIN_LENGTH) != 0) {
-                return -EINVAL;
-        }
-        return LOGIN_LENGTH;
+	if (!buff || len != LOGIN_LENGTH)
+		return -EINVAL;
+	if (copy_from_user(text, buff, len) > 0)
+		return -EIO;
+	if (memcmp(text, LOGIN, LOGIN_LENGTH) != 0)
+		return -EINVAL;
+
+	return LOGIN_LENGTH;
 }
 
-static struct file_operations id_file_ops = {
-        .owner = THIS_MODULE,
-        .read = id_read,
-        .write = id_write,
+static const struct file_operations id_file_ops = {
+	.owner = THIS_MODULE,
+	.read = id_read,
+	.write = id_write,
 };
 
 static ssize_t jiffies_read(struct file *f, char __user *buff, size_t len, loff_t *offset)
 {
-        char jiffies_buffer[32];
-        int written = snprintf(jiffies_buffer, sizeof(jiffies_buffer), "%lu", get_jiffies_64());
+	char jiffies_buffer[32];
+	int written = snprintf(jiffies_buffer, sizeof(jiffies_buffer), "%llu", get_jiffies_64());
 
-        return simple_read_from_buffer(buff, len, offset, jiffies_buffer, written);
+	return simple_read_from_buffer(buff, len, offset, jiffies_buffer, written);
 }
 
-static struct file_operations jiffies_file_ops = {
-        .owner = THIS_MODULE,
-        .read = jiffies_read,
+static const struct file_operations jiffies_file_ops = {
+	.owner = THIS_MODULE,
+	.read = jiffies_read,
 };
 
 static size_t foo_length;
@@ -71,46 +70,45 @@ static DEFINE_MUTEX(foo_buffer_mutex);
 
 static ssize_t foo_read(struct file *f, char __user *buff, size_t len, loff_t *offset)
 {
-        if (!buff) {
-                return -EINVAL;
-        }
-        if (*offset >= foo_length) {
-                return 0;
-        }
+	if (!buff)
+		return -EINVAL;
+	if (*offset >= foo_length)
+		return 0;
 
-        mutex_lock(&foo_buffer_mutex);
+	mutex_lock(&foo_buffer_mutex);
 
-        size_t copy_length = min(foo_length - *offset, len);
-        size_t copied = copy_length - copy_to_user(buff, foo_buffer + *offset, copy_length);
-        *offset += copied;
+	size_t copy_length = min(foo_length - *offset, len);
+	size_t copied = copy_length - copy_to_user(buff, foo_buffer + *offset, copy_length);
 
-        mutex_unlock(&foo_buffer_mutex);
+	*offset += copied;
 
-        return (ssize_t)copied;
+	mutex_unlock(&foo_buffer_mutex);
+
+	return (ssize_t)copied;
 }
 
 static ssize_t foo_write(struct file *f, const char __user *buff, size_t len, loff_t *offset)
 {
-        if (!buff) {
-                return -EINVAL;
-        }
+	if (!buff)
+		return -EINVAL;
 
-        mutex_lock(&foo_buffer_mutex);
+	mutex_lock(&foo_buffer_mutex);
 
-        size_t copy_length = min(PAGE_SIZE - foo_length, len);
-        size_t copied = copy_length - copy_from_user(foo_buffer + foo_length, buff, copy_length);
-        foo_length += copied;
-        *offset = foo_length;
+	size_t copy_length = min(PAGE_SIZE - foo_length, len);
+	size_t copied = copy_length - copy_from_user(foo_buffer + foo_length, buff, copy_length);
 
-        mutex_unlock(&foo_buffer_mutex);
+	foo_length += copied;
+	*offset = foo_length;
 
-        return (ssize_t)copied;
+	mutex_unlock(&foo_buffer_mutex);
+
+	return (ssize_t)copied;
 }
 
-static struct file_operations foo_file_ops = {
-        .owner = THIS_MODULE,
-        .read = foo_read,
-        .write = foo_write,
+static const struct file_operations foo_file_ops = {
+	.owner = THIS_MODULE,
+	.read = foo_read,
+	.write = foo_write,
 };
 
 static struct dentry *debug_dir;
@@ -120,41 +118,42 @@ static struct dentry *foo_file;
 
 static int __init hellodebugfs_init(void)
 {
-        debug_dir = debugfs_create_dir("fortytwo", NULL);
-        if (!debug_dir) {
-                pr_error("Could not create debugfs directory 'fortytwo'\n");
-                return -PTR_ERR(debug_dir);
-        }
+	debug_dir = debugfs_create_dir("fortytwo", NULL);
+	if (!debug_dir) {
+		pr_err("Could not create debugfs directory 'fortytwo'\n");
+		return -PTR_ERR(debug_dir);
+	}
 
-        id_file = debugfs_create_file("id", 0666, debug_dir, NULL, &id_file_ops);
-        if (!id_file) {
-                pr_error("Could not create 'id' file\n");
-                debugfs_remove_recursive(debug_dir);
-                return -PTR_ERR(id_file);
-        }
+	id_file = debugfs_create_file("id", 0666, debug_dir, NULL, &id_file_ops);
+	if (!id_file) {
+		pr_err("Could not create 'id' file\n");
+		debugfs_remove_recursive(debug_dir);
+		return -PTR_ERR(id_file);
+	}
 
-        jiffies_file = debugfs_create_file("jiffies", 0444, debug_dir, NULL, &jiffies_file_ops);
-        if (!jiffies_file) {
-                pr_error("Could not create 'jiffies' file\n");
-                debugfs_remove_recursive(debug_dir);
-                return -PTR_ERR(jiffies_file);
-        }
+	jiffies_file = debugfs_create_file("jiffies", 0444, debug_dir, NULL, &jiffies_file_ops);
+	if (!jiffies_file) {
+		pr_err("Could not create 'jiffies' file\n");
+		debugfs_remove_recursive(debug_dir);
+		return -PTR_ERR(jiffies_file);
+	}
 
-        foo_file = debugfs_create_file("foo", 0644, debug_dir, NULL, &foo_file_ops);
-        if (!foo_file) {
-                pr_error("Could not create 'foo' file\n");
-                debugfs_remove_recursive(debug_dir);
-                return -PTR_ERR(foo_file);
-        }
+	foo_file = debugfs_create_file("foo", 0644, debug_dir, NULL, &foo_file_ops);
+	if (!foo_file) {
+		pr_err("Could not create 'foo' file\n");
+		debugfs_remove_recursive(debug_dir);
+		return -PTR_ERR(foo_file);
+	}
 
-        pr_info("Initialized debugfs module\n");
-        return 0;
+	pr_info("Initialized debugfs module\n");
+
+	return 0;
 }
 
 static void __exit hellodebugfs_exit(void)
 {
-        debugfs_remove_recursive(debug_dir);
-        pr_info("Cleaned up debugfs module\n");
+	debugfs_remove_recursive(debug_dir);
+	pr_info("Cleaned up debugfs module\n");
 }
 
 module_init(hellodebugfs_init);
---
+++ ex07/test_id.c
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int file = open("/sys/kernel/debug/fortytwo/id", O_RDWR);
+
+	if (file == -1) {
+		printf("ERROR: could not open /sys/kernel/debug/fortytwo/id (%s)\n", strerror(errno));
+		return 1;
+	}
+
+	int status = 0;
+
+	int n = write(file, "abc", 3);
+
+	if (n != -1 || errno != EINVAL) {
+		printf("ERROR: write 'abc' returned %d\n", n);
+		status = 1;
+	}
+
+	n = write(file, "soumanso", strlen("soumanso"));
+	if (n != strlen("soumanso")) {
+		printf("ERROR: write 'soumanso' returned %d (%s)\n", n, strerror(errno));
+		status = 1;
+	}
+
+	n = write(file, "souman", strlen("souman"));
+	if (n != -1 || errno != EINVAL) {
+		printf("ERROR: write 'souman' returned %d\n", n);
+		status = 1;
+	}
+
+	char buff[100];
+
+	n = read(file, buff, 3);
+	if (n != 3) {
+		printf("ERROR: read 3 bytes returned %d\n", n);
+		status = 1;
+	}
+	if (memcmp(buff, "soumanso", 3) != 0) {
+		printf("ERROR: buff == '%.*s'\n", 3, buff);
+		status = 1;
+	}
+
+	n = read(file, buff + 3, sizeof(buff) - 3);
+	if (n != strlen("soumanso") - 3) {
+		printf("ERROR: read returned %d\n", n);
+		status = 1;
+	}
+	if (memcmp(buff, "soumanso", strlen("soumanso")) != 0) {
+		printf("ERROR: buff == '%.*s'\n", (int)strlen("soumanso"), buff);
+		status = 1;
+	}
+
+	if (status == 1)
+		printf("TESTS FAILED\n");
+	else
+		printf("TESTS PASSED\n");
+
+	return status;
+}
